476afbf2fb6931647161c651fe4ff9ea8ec7a28c
Burke Mamlin <burke@openmrs.org>
2016-11-11 15:28:19 -0500

    initial bug fixes

--- admin/js/env.js
+++ admin/js/env.js
@@ -2,8 +2,8 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.API = 'http://localhost/api';
-//  window.__env.API = 'http://10.0.11.161:3000';
+//  window.__env.API = 'http://localhost/api';
+  window.__env.API = 'https://treasurehunt.regenstrief.org/api';
 
   // Key for auth token in local storage
   window.__env.LOCAL_TOKEN_KEY = 'org.regenstrief.treasurehunt';
