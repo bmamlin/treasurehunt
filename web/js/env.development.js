9e780497a4c5abcde0867d7286cb6e6dcb3d4d63
Burke Mamlin <burke@openmrs.org>
2017-10-27 02:00:30 -0400

    Support dev and prod environs

--- web/js/env.development.js
+++ web/js/env.development.js
@@ -3,7 +3,6 @@
 
   // API url
   window.__env.API = 'http://localhost/api';
-//  window.__env.API = 'https://treasurehunt.regenstrief.org/api';
 
   // Key for auth token in local storage
   window.__env.LOCAL_TOKEN_KEY = 'org.regenstrief.treasurehunt';
---
+++ web/js/env.production.js
@@ -0,0 +1,11 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.API = 'https://treasurehunt.regenstrief.org/api';
+
+  // Key for auth token in local storage
+  window.__env.LOCAL_TOKEN_KEY = 'org.regenstrief.treasurehunt';
+
+  window.__env.debug = true;
+}(this));
